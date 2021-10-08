3254878022c4bc0611e7d11f15fe8bf17c0538c3
ichsandafa21 <ichsandafa21@gmail.com>
2021-10-08 21:18:08 +0700

    ADD image slider

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from './components/header'
+import Header from './components/Header'
 import './App.css';
 import Home from './components/Home';
 function App() {
